b9fd48914244ba3188c246c1f0f33131964d4a54
rahul sohal <rahul@iandwe.in>
2017-11-05 16:05:03 +0530

    updated mlab settings

--- app.js
+++ app.js
@@ -12,17 +12,18 @@ var app = express();
 const route = require('./routes/routes');
 
 //Connection to mongoDB 
-mongoose.connect('mongodb://rsohal:quiz2017@localhost:27017/quizdata');
+mongoose.connect('mongodb://rsohal:quiz2017@ds149335.mlab.com:49335/quizdata');
+//mongodb://<dbuser>:<dbpassword>@ds149335.mlab.com:49335/quizdata
 
 //on connection
 mongoose.connection.on('connected', function(){
-	console.log('Connected to database 27017');
+	console.log('Connected to database');
 });
 
 //on error connecting
 mongoose.connection.on('error', function(err){
 	if(err){
-		console.log('Error connecting to database:27017');	
+		console.log('Error connecting to database');	
 	}
 });
 
